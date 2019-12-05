c70f5e63582079bb38e96f1f9138d2cea3e7ab54
Alexander <teploff.a@mail.ru>
2019-12-05 18:25:52 +0300

    make first hw

--- hw_1/main.go
+++ hw_1/main.go
@@ -1,2 +1,21 @@
-package hw_1
+package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+	"time"
+)
+
+func main() {
+	currentTime := time.Now()
+	fmt.Println("Текущее время:", currentTime)
+
+	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		code, _ := os.Stderr.WriteString(err.Error())
+		os.Exit(code)
+	} else {
+		fmt.Println("Точное время:", exactTime)
+	}
+}
