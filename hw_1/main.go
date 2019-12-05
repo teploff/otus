a98ab9ff7df0458053ca4d47c603ed825e7ea5d0
Alexander <teploff.a@mail.ru>
2019-12-05 19:10:37 +0300

    refactoring

--- hw_1/main.go
+++ hw_1/main.go
@@ -7,11 +7,13 @@ import (
 	"time"
 )
 
+const hostName = "0.beevik-ntp.pool.ntp.org"
+
 func main() {
 	currentTime := time.Now()
 	fmt.Println("Текущее время:", currentTime)
 
-	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	exactTime, err := ntp.Time(hostName)
 	if err != nil {
 		code, _ := os.Stderr.WriteString(err.Error())
 		os.Exit(code)
